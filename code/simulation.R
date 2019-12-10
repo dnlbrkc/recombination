fc2b5d4e689048c22592d28f3a4542c499269808
Daniel Barkoczi <dnlbrkc@gmail.com>
2019-12-10 12:44:24 +0100

    new sim

--- code/simulation.R
+++ code/simulation.R
@@ -2,35 +2,28 @@ packages <- c('tictoc',
               'profvis',
               'compositions',
               'tidyverse',
-              'here')
+              'here',
+              'gtools',
+              'fGarch',
+              'tidyverse')
 
 lapply(packages, require, character.only = TRUE)
 
+source("setup/NK_function.R")
+
 na <- 10          # number of agents
-<<<<<<< HEAD
-N <- 16            # string length
-K <- c(0, 2, 6, 8) # task complexities
-repmax <- 1         # number of replications
+# N <- 16            # string length
+# K <- c(0, 2, 6, 8) # task complexities
+# 1 <- 1         # number of replications
 nK <- length(K)
 local=1
-#f <- as.integer( commandArgs(TRUE)[1])
 
-for(f in 1:2){ #repetitions
-
-#options(warn=-1)
-  
-
-# path_base = "/home/ubuntu/recombination"
-# path_scapes = file.path(path_base,'landscapes')
-# setwd(path_base)
-=======
 N <- 10           # string length
 K <- c(0, 4, 8) # task complexities
 repmax <- 1         # number of replications
 nK <- length(K)
 local=1
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
-
+reps=30
 # load landscapes
 name <- paste0(here("landscapes/"),"LS_",N,'.Rds')
 landscape <- read_rds(name)
@@ -42,42 +35,55 @@ groups <- matrix(groups, nrow=length(groups), ncol=2)
 groups[,2] <- na - groups[,1]
 ng <- nrow(groups)
 
-
-<<<<<<< HEAD
-# REVIEW 1 ----------------------------------------------------------------
-#function of N?????
-ch_extents <- c(0.05, 0.25, 0.5, 0.75, 1)  #delete 0.95 in case of n=10
-#simchoices <- c(0,1, 11, 21, 51, 81, 91)
-simchoices <- c(0,1,3,4,6,8,9)
-=======
 ch_extents <- c(0.05, 0.25, 0.5, 0.75, 1)  #delete 0.95 in case of n=10
 #simchoices <- c(0,1, 11, 21, 51, 81, 91)
 simchoices <- c(0,2,3,4,5,6,7,8,9)
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
 # tmax may depend on K 
 timesteps <- rep(30,nK)
 
 # initialize payoffs
-payoffs <- matrix(nrow=nsol, ncol=nK)
+payoffs <- array(0,dim=c(nsol,nK,reps))
+# load payoffs
 
-  # load payoffs
-  c=0
-  for( ik in K){
-    c=c+1
-    name <- paste0(here("landscapes/"),"landscape_",N,'_',ik,'.Rds')
-    temp <- read_rds(name)
-    payoffs[,c] <- temp[,2]
+c=0
+for( ik in K){
+  c=c+1
+  for(frep in 1:reps){
+    LS<-permutations(2,N,v=c(0,1),repeats.allowed=TRUE)
+    LS<-as.data.frame(LS)
+    if (ik==0){
+      depends <- as.vector(1:N)
+      values <- replicate(N,round(runif(2,0,1),1))
+      fitness <- values
+    } else {
+      # INSTEAD OF depends <- rbind(1:N,replicate(N,sample(c(1:N),K,replace=F)))
+      depends <- matrix(nrow = ik+1, ncol = N)
+      for(i in 1:N) {
+        string <- c(1:N)
+        string <- string[-i]
+        depends[,i] <- c(i,sample(string,ik,replace=F))
+      }
+      #
+      combinations <- permutations(2,ik+1,v=c(0,1),repeats.allowed=TRUE)
+      values <- replicate(N,round(runif(nrow(combinations),0,1),1))
+      fitness <- cbind(combinations,values)
+    }
+    
+    land <- generate_landscape(N,ik,LS,fitness,depends)
+    payoffs[,c,frep] <- (land[,N+1]/max(land[,N+1]))^8
   }
-for(f in 1:100){ #repetitions  
+}
+
+
+
+for(f in 1:reps){ #repetitions  
   # loop over different ways neighbors selected (0=rand, 1=10% most, 11=20% most etc )
   # REVIEW 2----------------------------------------------------------------
-<<<<<<< HEAD
-  ro=1
-  for(simchoice in simchoices[-1]) {  #with 10: 0,1,3,4,6,8,9 ? 
-=======
+  
+  
   ro=0
   for(simchoice in simchoices) {  #with 10: 0,1,3,4,6,8,9 ? 
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
+    
     ro=ro+1
     # loop over different Ks
     for( ik in 1:nK ) {
@@ -87,12 +93,8 @@ for(f in 1:100){ #repetitions
       
       for( cex in 1:length(ch_extents)){
         tic()
-<<<<<<< HEAD
-        # REVIEW 3----------------------------------------------------------------
-        ch_extent <- ceiling(ch_extents[cex]*N) # , matlab uses half to integer, R uses half to even integer
-=======
+        
         ch_extent <- ceiling(ch_extents[cex]*N) 
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
         
         # initialize storage variables
         pay_g <- matrix(NA, nrow=ng, ncol=tmax+1)
@@ -105,169 +107,109 @@ for(f in 1:100){ #repetitions
                          unlist(replicate(groups[g,2], 2)) )
           a_groups <- sample(a_groups) # order of groups randomized
           
-          pay_t <- matrix(NA, repmax, tmax)
-          dif_true_avg <- matrix(NA, repmax, tmax)
-          cex_true_avg <- matrix(NA, repmax, tmax)
-          uniqueSols_avg <- matrix(NA,repmax,tmax)
-          for( rep in 1:repmax ){
-            # starting solutions of agents
-            sol_no <- ceiling(runif(na)*nsol)
-            a_sols <- landscape[sol_no, ]
+          pay_t <- matrix(NA, 1, tmax)
+          dif_true_avg <- matrix(NA, 1, tmax)
+          cex_true_avg <- matrix(NA, 1, tmax)
+          uniqueSols_avg <- matrix(NA,1,tmax)
+          # starting solutions of agents
+          sol_no <- ceiling(runif(na)*nsol)
+          a_sols <- landscape[sol_no, ]
+          
+          # storage for this replication
+          dif_true <- matrix(NA, nrow=na, ncol=tmax)
+          cex_true <- matrix(NA, nrow=na, ncol=tmax)
+          uniqueSols <- vector()
+          # loop over tmax time steps
+          for( t in 1:tmax ){
+            # temporary matrices to store new solutions - needed for simultaneous updating
+            sol_no_new <- replicate(length(sol_no), NA)
+            a_sols_new <- matrix(NA, nrow=nrow(a_sols), ncol=ncol(a_sols))
             
-            # storage for this replication
-            dif_true <- matrix(NA, nrow=na, ncol=tmax)
-            cex_true <- matrix(NA, nrow=na, ncol=tmax)
-            uniqueSols <- vector()
-            # loop over tmax time steps
-            for( t in 1:tmax ){
-              # temporary matrices to store new solutions - needed for simultaneous updating
-              sol_no_new <- replicate(length(sol_no), NA)
-              a_sols_new <- matrix(NA, nrow=nrow(a_sols), ncol=ncol(a_sols))
+            
+            if(simchoice >= 1){
+              distances <- matrix(NA, nrow=na, ncol=na)
+              for( iia in 1:na ){
+                for( ja in 1:na ){
+                  distances[iia, ja] <- sum(a_sols[iia, ] != a_sols[ja, ])
+                }
+              }
+            }
+            
+            # order of agents random
+            ord <- sample(1:na)
+            # loop over agents - learning depends on the group - some update some not
+            for( ia in ord ){
+              if( simchoice == 0 ) { # random choice
+                neighbor <- sample(1:na,1);
+              } else { # most similar neighbor
+                x <- sort.int(distances[ia,], index.return=T)
+                range <- sample(simchoices[-1][ro-1]:simchoice,1)
+                neighbor <- x$ix[range]
+              }
               
-              # calculate similarities between current solutions
-              # REVIEW 4----------------------------------------------------------------
-              if(simchoice >= 1){
-                distances <- matrix(NA, nrow=na, ncol=na)
-                for( iia in 1:na ){
-                  for( ja in 1:na ){
-                    distances[iia, ja] <- sum(a_sols[iia, ] != a_sols[ja, ])
-                  }
+              test <- a_sols[ia,] # my current solution
+              neighbor_sol <- a_sols[neighbor, ] # find neighbor's solution
+              dif_true[ia, t] <- sum( test != neighbor_sol)/N
+              
+              disbits <- which(neighbor_sol != test)
+              cex_true[ia, t] <- length(disbits)/N 
+              
+              if( length(disbits) >= ch_extent ){ 
+                disbits_take <- disbits[1:ch_extent]
+                cex_true[ia, t] <- ch_extent/N 
+                
+              }
+              if( length(disbits) < ch_extent ){
+                disbits_take <- disbits
+                cex_true[ia, t] <- length(disbits_take)/N  #record
+              } # end of finding dissimilar bits
+              
+              if( a_groups[ia] == 1){ # implement mutation
+                #change ch_extent number of bits
+                test <- unlist(a_sols[ia,]) # my current solution
+                position <- sample(1:N,ch_extent)#
+                test[position] <- abs(test[position]-1) # flip the bits
+                
+                indx <- 1 + unbinary(paste(test, collapse=''))
+                if( payoffs[indx, ik,f] > payoffs[sol_no[ia], ik,f] ) { # find payoff of the new string
+                  a_sols_new[ia, ] <- as.numeric(test) # take it if better
+                  sol_no_new[ia] <- indx
                 }
               }
               
-              # order of agents random
-              ord <- sample(1:na)
-              # loop over agents - learning depends on the group - some update some not
-              for( ia in ord ){
-                # find more or less similar neighbors for both mutation and recombination
-                # (for mutation it is not used except to later be able to compare with
-                # recombination at the same level of similarity)
-                if( simchoice == 0 ) { # random choice
-                  neighbor <- ceiling(runif(1)*na);
-                } else { # most similar neighbor
-                  #x <- sort.int(distances[ia,], index.return=T)
-                  #tempn <- ceiling(runif(1)*na/10)
-                  # picks one neighbor in the 10-agent range    starting from lowest extentsim
-                  #neighbor <- x$ix[simchoice-1+tempn]
-                  x <- sort.int(distances[ia,], index.return=T)
-                  range <- sample(simchoices[-1][ro-1]:simchoice,1)
-                  neighbor <- x$ix[range]
-<<<<<<< HEAD
-  # ????????               neighbor <- x$ix[1:simchoice] #5th most similar, anyone in 41:50 randomly
-=======
-                  #neighbor <- x$ix[1:simchoice] #5th most similar, anyone in 41:50 randomly
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
-                  #neighbor <- x$ix[ round((simchoice/10)+1) ]
-                  
+              else if ( a_groups[ia] == 2 ){ # can it be improved by recombination
+                test[disbits_take] <- neighbor_sol[disbits_take]
+                
+                #indx <- which(apply(landscape, 1, function(row) all( row == test)))
+                indx <- 1 + unbinary(paste(test, collapse=''))
+                if( payoffs[indx, ik,f] > payoffs[sol_no[ia], ik,f] ) { # find payoff of the new string
+                  a_sols_new[ia, ] <- as.numeric(test) # take it if better
+                  sol_no_new[ia] <- indx
                 }
                 
-                test <- a_sols[ia,] # my current solution
-                neighbor_sol <- a_sols[neighbor, ] # find neighbor's solution
-                
-                # record actual difference from neighbor (TRUEDIF) --> this is
-                # the upper limit  on extent of recombination
-                
-                dif_true[ia, t] <- sum( test != neighbor_sol)/N
-                
-                # for both mut and recomb, implement finding dissimilar bits
-                # for mutation it doesn't matter but we still want to have it recoded
-                disbits <- which(neighbor_sol != test)
-                # this is the actual extent of recombination (TRUCEX)
-                cex_true[ia, t] <- length(disbits)/N 
-                
-                if( length(disbits) >= ch_extent ){ # if enough dissimilar bits
-                  # this is the actual extent of recombination, for when cex<available bits
-                  #?????????????
-            ###############      #ch_extents[cex] ch_extent[cex] * length(disbits)
-                  disbits_take <- disbits[1:ch_extent]
-                  #disbits_take <- disbits[ceiling( runif(ch_extent*length(disbits))) ]
-<<<<<<< HEAD
-                  cex_true[ia, t] <- length(disbits)/N 
-=======
-                  cex_true[ia, t] <- ch_extent/N #length(disbits)/N 
->>>>>>> 2c7724edc74513180730c4176604ec2b9ac639ba
-                }
-                # if too few dissimilar bits
-                if( length(disbits) < ch_extent ){
-                  # take what you have and sample the rest from the remaining bits
-                  # Bug here? setdiff is a list, compare to scalar
-                  #????????????????????????? take whatever is there, record change extent
-                  # if( length(setdiff(1:N, disbits)) >= ch_extent-length(disbits) ){
-                  #   disbits_take <- c(disbits, sample(setdiff(1:N, disbits), ch_extent-length(disbits)))
-                  # } else {
-                  #   disbits_take <- c(disbits, sample(1:N, ch_extent-length(disbits)))
-                  # }
-                  disbits_take <- disbits
-                  cex_true[ia, t] <- length(disbits_take)/N  #record
-                } # end of finding dissimilar bits
-                
-                if( a_groups[ia] == 1){ # implement mutation
-                  # use ch_extent that can be achieved by recombination
-# Beginning ---------------------------------------------------------------
-                  #ch_extent_true <- round(cex_true[ia, t]*N) # : round() differes, s.a.
-                  #ch_extent_true <- N
-# End ---------------------------------------------------------------------
-
-                  #change ch_extent number of bits
-                  test <- unlist(a_sols[ia,]) # my current solution
-                  position <- sample(1:N,ch_extent)#ceiling(runif(1)*N) # find random position
-                  
-                  #take_part <- position:(position+ch_extent_true-1) # determine mutation string
-                  #take_part[take_part>N] <- take_part[take_part>N] - N # edit to cross start and boundaries
-                  
-                  test[position] <- abs(test[position]-1) # flip the bits
-                  
-                  #indx <- which(apply(landscape, 1, function(row) all( row == test)) == TRUE)
-                  
-                  indx <- 1 + unbinary(paste(test, collapse=''))
-                  if( payoffs[indx, ik] > payoffs[sol_no[ia], ik] ) { # find payoff of the new string
-                    a_sols_new[ia, ] <- as.numeric(test) # take it if better
-                    sol_no_new[ia] <- indx
-                  }
-                }
-                
-                else if ( a_groups[ia] == 2 ){ # can it be improved by recombination
-                  test[disbits_take] <- neighbor_sol[disbits_take]
-                  
-                  #indx <- which(apply(landscape, 1, function(row) all( row == test)))
-                  indx <- 1 + unbinary(paste(test, collapse=''))
-                  if( payoffs[indx, ik] > payoffs[sol_no[ia], ik] ) { # find payoff of the new string
-                    a_sols_new[ia, ] <- as.numeric(test) # take it if better
-                    sol_no_new[ia] <- indx
-                  }
-                  
-                } # end of social learning in groups
-              } # end of looping over agents
-              
-              # update a_sols after all agents made decisions
-              a_sols[complete.cases(a_sols_new),] <- a_sols_new[complete.cases(a_sols_new),]
-              sol_no[complete.cases(sol_no_new)] <- sol_no_new[complete.cases(sol_no_new)]
-              
-              # save current payoff - sum of payoffs of all agents
-              pay_t[rep, t] <- sum( payoffs[sol_no, ik])
-              
-              #track unique solutions over time
-              uniqueSols[t] <-length(unique(sol_no))
-            } # end looping through time
+              } # end of social learning in groups
+            } # end of looping over agents
             
-            # save average difference between models
-            dif_true_avg[rep, ] <- colMeans(dif_true) #: check mean
-            cex_true_avg[rep, ] <- colMeans(cex_true)
-            uniqueSols_avg[rep,] <- uniqueSols
-            #pay_r[rep,] <- pay_t[tmax]
-          } # end of looping over replications, save all time steps
+            # update a_sols after all agents made decisions
+            a_sols[complete.cases(a_sols_new),] <- a_sols_new[complete.cases(a_sols_new),]
+            sol_no[complete.cases(sol_no_new)] <- sol_no_new[complete.cases(sol_no_new)]
+            
+            # save current payoff - sum of payoffs of all agents
+            pay_t[1, t] <- sum( payoffs[sol_no, ik,f])
+            
+            #track unique solutions over time
+            uniqueSols[t] <-length(unique(sol_no))
+          } # end looping through time
           
-          if( repmax == 1 ){
-            pay_g[g, ] <- c(groups[g,2], pay_t)
-            truedif_g[g, ] <- c(groups[g,2], dif_true_avg)
-            truecex_g[g, ] <- c(groups[g,2], cex_true_avg)
-            unique_g[g,] <-c(groups[g,2], uniqueSols_avg)
-          } else {
-            pay_g[g, ] <- c(groups[g,2], mean(pay_t, na.rm=T))
-            truedif_g[g, ] <- c(groups[g,2], mean(dif_true_avg, na.rm=T))
-            truecex_g[g, ] <- c(groups[g,2], mean(cex_true_avg, na.rm=T))
-            unique_g[g,] <-c(groups[g,2], mean(uniqueSols_avg,na.rm=T))
-          }
+          # save average difference between models
+          dif_true_avg[1, ] <- colMeans(dif_true) #: check mean
+          cex_true_avg[1, ] <- colMeans(cex_true)
+          uniqueSols_avg[1,] <- uniqueSols
+          
+          pay_g[g, ] <- c(groups[g,2], pay_t)
+          truedif_g[g, ] <- c(groups[g,2], dif_true_avg)
+          truecex_g[g, ] <- c(groups[g,2], cex_true_avg)
+          unique_g[g,] <-c(groups[g,2], uniqueSols_avg)
         } # end of looping through all possible groups
         
         #setwd("results")
